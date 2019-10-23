92daeaf1793e905b457ce0fa0eb467306a492bf3
Josh Sawyers <jogrsa@gmail.com>
2019-10-23 01:12:32 -0700

    Inital updates to fix headers and timestamp for pcap extracts.

--- EtlToCap/Program.cs
+++ EtlToCap/Program.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EtlToCap
 {
@@ -15,7 +11,7 @@ namespace EtlToCap
         {
             if (args.Length ==0)
             {
-                Console.WriteLine("Usage: EtlCap <source ETL file> <destination pcap file>");
+                Console.WriteLine("Usage: EtlToCap <source ETL file> <destination pcap file>");
                 return;
             }
             Console.WriteLine($"Converting File {args[0]} to {args[1]}");
@@ -26,16 +22,15 @@ namespace EtlToCap
         public static long ConvertEtlToPcap(string source, string destination, UInt32 maxPacketSize)
         {
             int result = 0;
-            var networkTrace = new Guid("{00000001-0000-0000-0000-000000000000}");
             using (BinaryWriter writer = new BinaryWriter(File.Open(destination, FileMode.Create)))
             {
 
-                UInt32 magic_number = 0xa1b2c3d4;
+                UInt32 magic_number = 0xa1b23c4d; // Swap to nanosecond pcap format to get the most resolution from ETL
                 UInt16 version_major = 2;
                 UInt16 version_minor = 4;
-                Int32 thiszone = 0;
+                Int32 thiszone = 0; // Set capture timezone to UTC
                 UInt32 sigfigs = 0;
-                UInt32 snaplen = maxPacketSize;
+                UInt32 snaplen = 0xffff; // Set snaplen to 65535
                 UInt32 network = 1; // LINKTYPE_ETHERNET
 
                 writer.Write(magic_number);
@@ -53,12 +48,13 @@ namespace EtlToCap
                     {
                         using (record)
                         {
-                            if (record.ActivityId == networkTrace)
+                            if (record.ProviderName == "Microsoft-Windows-NDIS-PacketCapture") // Only process network packets.
                             {
                                 result++;
                                 DateTime timeCreated = (DateTime)record.TimeCreated;
-                                UInt32 ts_sec = (UInt32)((timeCreated.Subtract(new DateTime(1970, 1, 1))).TotalSeconds);
-                                UInt32 ts_usec = (UInt32)(((timeCreated.Subtract(new DateTime(1970, 1, 1))).TotalMilliseconds) - ((UInt32)((timeCreated.Subtract(new DateTime(1970, 1, 1))).TotalSeconds * 1000))) * 1000;
+                                DateTime normTime = timeCreated.ToUniversalTime(); //normalize time to UTC to match pcap header.
+                                UInt32 ts_sec = (UInt32)((normTime.Subtract(new DateTime(1970, 1, 1))).TotalSeconds);
+                                UInt32 ts_usec = Convert.ToUInt32(normTime.ToString("fffffff")) * 100; //ETL only has 7dp, pad to 9dp.
                                 UInt32 incl_len = (UInt32)record.Properties[2].Value;
                                 if (incl_len > maxPacketSize)
                                 {
